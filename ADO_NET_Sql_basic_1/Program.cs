7d2ec61c87d8aab465aebeb265504c07e1d35327
Peter Kubala <kubala.peter.402@gmail.com>
2021-03-04 22:04:24 +0100

    Add functionalty for read all data with SqlDataReader to console.

--- ADO_NET_Sql_basic_1/Program.cs
+++ ADO_NET_Sql_basic_1/Program.cs
@@ -62,13 +62,42 @@ using System.Data.SqlClient;
         }
     }
 
+    public void printTableData(string tableName, string connstr)
+    {
+        SqlConnection conn;
+        int rowCount = 0;
+        try
+        {
+            conn = new SqlConnection(connstr);
+            SqlCommand cmd = new SqlCommand("select COUNT(*) from " + tableName, conn);
+            conn.Open();
+            rowCount = (int)cmd.ExecuteScalar();
+            SqlCommand cmdRead = new SqlCommand("select *from " + tableName, conn);
+            SqlDataReader reader = cmdRead.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Id"] + " " +reader["Name"]+ " " + reader["email"]);
+            }
+
+            conn.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+
 
     static void Main(string[] args)
         {
         Program mainObj = new Program();
-        mainObj.createTable("TestTable", mainObj.connString);
-        mainObj.deleteDataAccordingId("TestTable", mainObj.connString, 3);
-        mainObj.deleteDataAccordingId("TestTable", mainObj.connString, 2);
+        //mainObj.createTable("TestTable", mainObj.connString);
+        //mainObj.deleteDataAccordingId("TestTable", mainObj.connString, 3);
+        //mainObj.deleteDataAccordingId("TestTable", mainObj.connString, 2);
+        //mainObj.insertToTable("TestTable", mainObj.connString, 2, "AAAA", "nasjjdhsaj");
+        //mainObj.insertToTable("TestTable", mainObj.connString, 3, "BBBB", "nasjjdhsaj");
+        mainObj.printTableData("TestTable", mainObj.connString);
         Console.WriteLine("Hello Peter Kubala is program in ADO.NET.");
         }
     }
