edb3e67edf910dc4ed28852698923abff7a98dba
Peter Kubala <kubala.peter.402@gmail.com>
2021-03-04 17:40:33 +0100

    Join database with application and connection open and close.

--- ADO_NET_Sql_basic_1/Program.cs
+++ ADO_NET_Sql_basic_1/Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Data.SqlClient;
+
+
     class Program
     {
+
+    public string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\kubal\\Documents\\Visual Studio 2015\\Projects\\Git_project\\ADO_NET_Sql_basic_1\\ADO_NET_Sql_basic_1\\TestDatabase.mdf;Integrated Security=True";
         static void Main(string[] args)
         {
+
+        using (SqlConnection sqlcon = new SqlConnection((new Program()).connString))
+        {
+            sqlcon.Open();
+            sqlcon.Close();
+        }
+
         Console.WriteLine("Hello Peter Kubala in ADO.NET.");
         }
     }
