09fddb6e99ea005757362eba10c3eae04a662432
Peter Kubala <kubala.peter.402@gmail.com>
2021-03-04 17:56:50 +0100

    Create function for make new table in TestDatabase, create TestTable in database.

--- ADO_NET_Sql_basic_1/Program.cs
+++ ADO_NET_Sql_basic_1/Program.cs
@@ -6,15 +6,33 @@ using System.Data.SqlClient;
     {
 
     public string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\kubal\\Documents\\Visual Studio 2015\\Projects\\Git_project\\ADO_NET_Sql_basic_1\\ADO_NET_Sql_basic_1\\TestDatabase.mdf;Integrated Security=True";
-        static void Main(string[] args)
-        {
 
-        using (SqlConnection sqlcon = new SqlConnection((new Program()).connString))
+    public void createTable(string tableName, string connstr)
+    {
+        SqlConnection conn;
+        try
         {
-            sqlcon.Open();
-            sqlcon.Close();
+            conn = new SqlConnection(connstr);
+            SqlCommand cmd = new SqlCommand("create table " + tableName + "(Id int not null, Name varchar(50), email varchar(50))", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Table " + tableName + " was created.");
+            conn.Close();
+        }catch(Exception e)
+        {
+            Console.WriteLine(e);
         }
 
+    }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+        Program mainObj = new Program();
+        mainObj.createTable("TestTable", mainObj.connString);
         Console.WriteLine("Hello Peter Kubala in ADO.NET.");
         }
     }
